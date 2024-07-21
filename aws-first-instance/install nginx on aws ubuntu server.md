4cba6e705e8e372368a3d4b7be2e4b667244785d
vnkt2005 <p.venkatesh.bdvt@gmail.com>
2024-07-21 22:59:08 +0530

    after aws instance created

---
+++ aws-first-instance/install nginx on aws ubuntu server.md
@@ -0,0 +1,6 @@
+# Install nginx on aws ubuntu 
+$sudo -i
+#apt-get install nginx
+#cd /var/www/html
+#ls
+#echo "This is Venkatesh" > index.nginx-debian.html
--- aws-first-instance/instance.tf
+++ aws-first-instance/instance.tf
@@ -1,24 +1,62 @@
-/*
+
 # creating ssh-key.
 resource "aws_key_pair" "key-tf" {
-    key_name = "key-tf"
-    public_key = file("${path.module}/id_rsa.pub")
+  key_name   = "key-tf"
+  public_key = file("${path.module}/id_rsa.pub")
 }
 
 output "keyname" {
-    value = "${aws_key_pair.key-tf.key_name}"
+  value = aws_key_pair.key-tf.key_name
 }
 
+
+# create aws instance
 resource "aws_instance" "web" {
-  ami           = "ami-04a81a99f5ec58529"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.key-tf.key_name}"
+  ami             = "ami-04a81a99f5ec58529"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.key-tf.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = "first-tf-instance"
   }
 }
-*/
+
+
+# create security group
+# terraform dynamic block
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 3306, 27017]
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
+
+output "securityGroupDetails" {
+  value = aws_security_group.allow_tls.id
+}
+
+
 
 output "hello" {
-    value = "the mysore"
+  value = "the mysore"
 }
